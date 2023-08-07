4e9da841e0cdde22c441b9b461d1d9da198efb9f
luc0liv <luoliveiradacunha@gmail.com>
2023-08-07 17:13:42 -0300

    feat: cria programa que lê valores e verifica se estão dentro do range

--- listaUm/exercicioSeis/Program.cs
+++ listaUm/exercicioSeis/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Leia um valor inteiro N. Este valor será a quan0dade de valores inteiros X que serão lidos em seguida. 
+//Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo,
+//mostrando essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para 
+//fora do intervalo)
+
+using System;
+
+namespace exercicioSeis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos valores serão lidos?");
+            bool valuesParse = int.TryParse(Console.ReadLine(), out int values);
+            int inValues = 0;
+            int outValues = 0;
+
+            if (!valuesParse) {
+                Console.WriteLine("Valor inválido");
+            } else {
+
+            for (int i = 0; i < values; i++)
+            {
+                Console.WriteLine("Digite um número");
+                bool valueXParse = int.TryParse(Console.ReadLine(), out int valueX);
+
+                if (!valueXParse)
+                {
+                    Console.WriteLine("Valor inválido");
+                } else
+                {
+                    if (valueX >= 10 && valueX <= 20)
+                    {
+                        inValues += 1;
+                    } else
+                    {
+                        outValues += 1;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{inValues} in");
+            Console.WriteLine($"{outValues} out");
+        }
+    }
+    }
+}
